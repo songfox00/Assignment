7fa3c32f89aca3b83341c1ea1e124bb97ef6a52e
Hyemin Song <69200763+songfox00@users.noreply.github.com>
2020-12-22 16:20:46 +0900

    Create Movie.java

---
+++ 자바프로그래밍/Final_project/src/Movie.java
@@ -0,0 +1,14 @@
+class Movie extends Item{
+	public Movie(String title, String dir, String act, String genre, String age, String year, String path, int star, String story, String review) {
+		this.title=title;
+		this.dir=dir;
+		this.act=act;
+		this.genre=genre;
+		this.age=age;
+		this.year=year;
+		this.star=star;
+		this.story=story;
+		this.review=review;
+		this.path=path;
+	}
+}
