a09023ac36aaa5c5a11ada96c80c4b09ec3c6397
Hyemin Song <69200763+songfox00@users.noreply.github.com>
2020-12-22 16:19:34 +0900

    Create Book.java

---
+++ 자바프로그래밍/Final_project/src/Book.java
@@ -0,0 +1,12 @@
+class Book extends Item{
+	public Book(String title, String dir, String publish , String year, String path, int star, String story, String review) {
+		this.title=title;
+		this.dir=dir;
+		this.publish=publish;
+		this.year=year;
+		this.star=star;
+		this.story=story;
+		this.review=review;
+		this.path=path;
+	}
+}
